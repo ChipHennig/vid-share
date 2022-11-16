0f46067747fdbfa7f6aa4e22967d375c07ddf40c
Christian Hennig <chip.hennig@gmail.com>
2022-11-16 11:18:19 -0600

    add mock api route for creating video links

---
+++ pages/api/create.js
@@ -0,0 +1,35 @@
+import { url } from 'yup'
+import redis, { databaseName } from 'lib/redis'
+import authenticate from 'lib/authenticate'
+
+export default authenticate(async (req, res) => {
+    try {
+        const { link } = req.body
+
+        let schema = url().required()
+        const isValid = await schema.isValid(link)
+
+        if (!isValid) {
+            throw new Error('Not a valid youtube link')
+        }
+
+        const { nickname, email, updated_at, ...user} = req.user
+
+        const VIDEO = {
+            link,
+            createdAt: Date.now(),
+            user,
+        }
+
+        await redis.hset(
+            databaseName,
+            email+link,
+            JSON.stringify(VIDEO)
+        )
+
+        res.json({body: 'success'})
+
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+})
